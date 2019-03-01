84b0992ae28e300a2efe6cec5f24d464dcf3eb14
roseline <guseod24@gmail.com>
2019-03-01 17:46:42 +0900

    movie app css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie'
+import { throws } from 'assert';
 
 class App extends Component {
 // Render: componentWillMount() -> render() -> componentDidMount()
@@ -10,23 +11,45 @@ class App extends Component {
   state = {  }
 
   componentDidMount() {
-    fetch('https://yts.am/api/v2/list_movies.json?sory_by=rating')
-    .then(potato = potato.json()) // status : 200 (Successed), ReadableStream : binary data
-    .then(json => console.log(json))
-    .catch(err => console.log(err)) 
+    this._getMovies();
+ 
   }
 
+  _getMovies = async () => {
+    const movies = await this._callApi() // await mode : this.setState : 
+    this.setState({
+      movies // callApi() 이 완료되기 전까지 실행되지 않는다. 
+    })
+  }
+
+  _callApi = () => {
+    // fetch 라는 이름의 promise 를 return 한다. 
+    return fetch('https://yts.am/api/v2/list_movies.json?sory_by=rating')
+    .then(potato => potato.json()) // status : 200 (Successed), ReadableStream : binary data
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
+  }
+
+
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map(movie => {
+      console.log(movie) // check what data is in Movie Data
+      return <Movie title={movie.title_english} 
+        poster={movie.medium_cover_image} 
+        key={movie.id} 
+        genres = {movie.genres}
+        synopsis = {movie.synopsis}
+        />
     })
     return movies
   }
 
   render() {
+    const { movies } = this.state;
+
     return (
-      <div className="App">
-        {this.state.movies? this._renderMovies() : 'Loading'}
+      <div className={movies ? "App" : "App--loading"}>
+        {movies ? this._renderMovies() : "Loading"}
         </div>
     );
   }
