b0eb4d845b54a257f7fcfc26b7b0c19d5f58bb27
roseline <guseod24@gmail.com>
2019-03-01 18:12:10 +0900

    Updates

---
+++ precache-manifest.133d3e65d45bb6ee605d48a6f768e6a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_movie_app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "77a583dbf7f86a63d009",
+    "url": "/react_movie_app/static/js/runtime~main.77a583db.js"
+  },
+  {
+    "revision": "00758abdbe8fc29ff861",
+    "url": "/react_movie_app/static/js/main.00758abd.chunk.js"
+  },
+  {
+    "revision": "5926aa6b538ccb98ce84",
+    "url": "/react_movie_app/static/js/2.5926aa6b.chunk.js"
+  },
+  {
+    "revision": "00758abdbe8fc29ff861",
+    "url": "/react_movie_app/static/css/main.f2430edd.chunk.css"
+  },
+  {
+    "revision": "068b86029190e9145e4769b33a04af3f",
+    "url": "/react_movie_app/index.html"
+  }
+];
