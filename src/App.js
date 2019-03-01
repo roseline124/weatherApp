7dd1bea3a2d9874a9b64ea92a2b44a0d8b5ec14e
roseline <guseod24@gmail.com>
2019-03-01 15:02:28 +0900

    setState

--- src/App.js
+++ src/App.js
@@ -3,37 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie'
 
-const movies = [
-  {
-    title : "Inception",
-    poster : 'https://images-na.ssl-images-amazon.com/images/I/912AErFSBHL._SY445_.jpg'
-  },
-  
-  {
-    title : "KimBockNam",
-    poster : 'http://image.cine21.com/resize/cine21/poster/2010/0817/M0010003_kimboknam_poster_main[X230,330].jpg',
-  }, 
-
-  {
-    title : "Shining",
-    poster : 'http://mblogthumb4.phinf.naver.net/MjAxODAzMjdfMjcy/MDAxNTIyMTQ0MjUzOTA5.W1y_24UWM53mTqsLuaM8vke0pqOex3I3e1WejC1RvIog.sUe7apWH-nN7nbLBhX50j-UlHr9fmP917L14OIahUYog.JPEG.cine_play/MV5BY2I4ZTNlMGUtZGFlYS00NDI4LWIxOTEtYTA0YTFhNTdmNzViXkEyXkFqcGdeQXVyNTgzMzU5MDI._V1_SY1000_CR0%2C0%2C666%2C1000_AL_.jpg?type=w800',
-  }, 
-]
-
-
 class App extends Component {
+
+  state = {
+    movies : [
+      {
+        title : "Inception",
+        poster : 'https://images-na.ssl-images-amazon.com/images/I/912AErFSBHL._SY445_.jpg'
+      },
+      
+      {
+        title : "KimBockNam",
+        poster : 'http://image.cine21.com/resize/cine21/poster/2010/0817/M0010003_kimboknam_poster_main[X230,330].jpg',
+      }, 
+  
+      {
+        title : "Shining",
+        poster : 'http://mblogthumb4.phinf.naver.net/MjAxODAzMjdfMjcy/MDAxNTIyMTQ0MjUzOTA5.W1y_24UWM53mTqsLuaM8vke0pqOex3I3e1WejC1RvIog.sUe7apWH-nN7nbLBhX50j-UlHr9fmP917L14OIahUYog.JPEG.cine_play/MV5BY2I4ZTNlMGUtZGFlYS00NDI4LWIxOTEtYTA0YTFhNTdmNzViXkEyXkFqcGdeQXVyNTgzMzU5MDI._V1_SY1000_CR0%2C0%2C666%2C1000_AL_.jpg?type=w800',
+      }, 
+    ]
+  }
 
 // Render: componentWillMount() -> render() -> componentDidMount()
 // Update componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
 
-  state = {
-    greeting : 'Hello!'
-  }
-
   componentDidMount() {
     setTimeout(() => {
       this.setState({
-        greeting : 'Hello again!'
+        movies : [
+        {
+          title : "Discovery", 
+          poster : 'https://upload.wikimedia.org/wikipedia/en/thumb/3/3c/The_Discovery_film_poster.jpg/220px-The_Discovery_film_poster.jpg',
+        }, 
+        ...this.state.movies
+      ] // moives 
       })
     }, 3000)
   }
@@ -41,8 +44,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.greeting}
-        {movies.map( (movie, index) => {
+        {this.state.movies.map( (movie, index) => {
           return <Movie title={movie.title} poster={movie.poster} key={index}/>
         }
         )
